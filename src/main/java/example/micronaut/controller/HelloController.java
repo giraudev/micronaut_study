b785ff0a4c9b958fd85628959b987172fe65c111
giraudev <giraudev@gmail.com>
2019-06-10 18:41:23 -0300

    first commit

---
+++ src/main/java/example/micronaut/controller/HelloController.java
@@ -0,0 +1,16 @@
+package example.micronaut.controller;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
+
+@Controller("/hello")
+public class HelloController {
+
+    @Get("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String index(){
+        return "Hello World";
+    }
+}
